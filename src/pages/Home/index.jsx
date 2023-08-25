9634a5587ed603eb2f74e2a5256236daee2f9e77
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2023-08-25 10:46:20 -0300

    Finish

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,56 @@
+import React, {useState, useEffect} from 'react';
 import {Card} from '../../components/Card'
 import './style.css'
 
 export function Home() {
+  const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name:'', avatar:''});
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br",{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/carlosbandelli')
+    .then(response => response.json())
+    .then(data => {
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+    })
+  },[])
+
   return (
     <div className="container">
-     <h1>Lista de presença</h1>
-     <input type="text" placeholder="Digite o seu nome..."/>
-     <button type="button">Adicionar</button>
-     <Card name="Carlos" time="10:55:25"/>
-     <Card name="arumaito" time="11:00:10"/>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+
+      </header>
+     <input type="text" placeholder="Digite o seu nome..." onChange={e => setStudentName(e.target.value)}/>
+     <button type="button" onClick={handleAddStudent}>Adicionar</button>
+      {   
+      students.map(student => (
+        <Card 
+        name={student.name} 
+        key={student.time}
+        time={student.time}
+        />      
+      ))       
+      }   
     </div>
   )
 }
